bbd5a2b81ea7146e54f0a535def31cf58ce7dbf1
Crystal <cl.crystal.li@gmail.com>
2016-02-23 20:57:03 -0500

    Settings goes to blank SettingsActivity.

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -35,6 +35,8 @@ public class DetailActivity extends ActionBarActivity {
         int id = item.getItemId();
 //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
